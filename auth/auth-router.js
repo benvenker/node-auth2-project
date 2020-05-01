be4266cd8d33699d26d29e0dabffebc77a1f4190
Ben Venker <bvemails@gmail.com>
2020-04-30 22:14:52 -0500

    implement login with cookie and jwt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,37 @@ const Users = require("../users/users-model");
 
 const router = express.Router();
 
-router.post("/login", async (req, res, next) => {});
+router.post("/login", async (req, res, next) => {
+  const authError = {
+    message: "Invalid credential",
+  };
+
+  try {
+    const { username, password } = req.body;
+
+    const user = await Users.findBy({ username }).first();
+    if (!user) {
+      return res.status(400).json(authError);
+    }
+
+    const payload = {
+      userId: user.id,
+      userRole: "admin",
+    };
+
+    // generate a new JWT
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+    // sends a Set-Cookie header witth the value of the token
+    res.cookie("token", token);
+
+    res.json({
+      message: `Welcome ${user.username}`,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post("/register", async (req, res, next) => {
   const user = req.body;
