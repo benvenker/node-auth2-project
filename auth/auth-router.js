0083cc3ded0c73c8a5632237aef4525728e27a50
Ben Venker <bvemails@gmail.com>
2020-04-30 21:43:18 -0500

    basic register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Users = require("../users/users-model");
 
 const router = express.Router();
 
-router.post("/login", (req, res) => {
-  console.log("Logged in");
-});
+router.post("/login", async (req, res, next) => {});
 
-router.post("/register", (req, res) => {
-  console.log("Registered!");
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (user) {
+      return res.status(400).json({
+        message: "You shall not pass!",
+      });
+    }
+
+    res.status(201).json(await Users.add(req.body));
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- users/users-model.js
+++ users/users-model.js
@@ -1,9 +1,20 @@
+const bcrypt = require("bcryptjs");
 const db = require("../data/db-config");
 
 module.exports = {
   find,
+  add,
+  findBy,
 };
 
 function find() {
-  return db("users");
+  return db("users").select("id", "username");
+}
+
+function add(user) {
+  return db("users").insert(user);
+}
+
+function findBy(filter) {
+  return db("users").select("id", "username", "password").where(filter);
 }
